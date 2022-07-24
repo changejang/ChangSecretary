3cbff27a6ca6c30b66445a79632d700f771176ec
changejang <changejang@gmail.com>
2022-07-24 14:10:12 +0900

    modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SchedulerModule } from './scheduler/scheduler.module';
+import { ToDoModule } from './to-do/to-do.module';
+import { NotificationModule } from './notification/notification.module';
+import { ReportModule } from './report/report.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), SchedulerModule, ToDoModule, NotificationModule, ReportModule],
   controllers: [AppController],
   providers: [AppService],
 })
