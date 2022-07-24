744d14f2621b8802b70011e25dcf20dc431ce2b8
changejang <changejang@gmail.com>
2022-07-24 16:33:54 +0900

    :sparkles: Add todo controller

--- src/to-do/to-do.controller.ts
+++ src/to-do/to-do.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { ToDoService } from './to-do.service';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('to-do')
+@ApiTags('To-do')
 export class ToDoController {
   constructor(private readonly toDoService: ToDoService) {}
+
+  @Get()
+  async getTodo() {}
+
+  @Get()
+  async getTodayTodo() {}
+
+  @Post()
+  async createTodo() {}
 }
